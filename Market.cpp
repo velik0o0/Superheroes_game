706643847a916d3bd55e1e381d8523dc495ee4a5
Veliko Fotev <77541335+velik0o0@users.noreply.github.com>
2023-06-28 14:05:27 +0200

    Update Market.cpp

--- Market.cpp
+++ Market.cpp
@@ -3,15 +3,33 @@
 #include <vector>
 #include "Market.h"
 using namespace std;
+
 Market::Market() = default;
 
 int Market::getNumHeroes() const {
     return heroes.size();
 }
+//!!
+bool Market::removeHero(const std::string& heroNickname) {
+    auto it = std::find_if(heroes.begin(), heroes.end(), [&heroNickname](const Hero& hero) {
+        return hero.getNickname() == heroNickname;
+        });
 
+    if (it != heroes.end()) {
+       heroes.erase(it);
+        return true;
+    }
+
+    return false;
+}
 void Market::addHero(const Hero& hero) {
     heroes.push_back(hero);
-    std::cout << hero.getNickname() << " successfully added to the Market" << std::endl;
+    std::cout << hero.getNickname() << " successfully added to the Market" << std::endl;  
+    std::cin.ignore();
+}
+void Market::addHeroToArch(const Hero& hero) {
+    heroes.push_back(hero);
+   
 }
 
 void Market::printMarket() const {

64954946f4988944262352959a3e00c84fdfeb45
Veliko Fotev <77541335+velik0o0@users.noreply.github.com>
2023-06-28 14:05:40 +0200

    Update Market.h

--- Market.h
+++ Market.h
@@ -13,10 +13,12 @@ public:
 
 	int getNumHeroes() const;
 	const std::vector<Hero>& getHeroes() const;
-
 	
+	bool removeHero(const std::string& heroNickname);
+		
 	void addHero(const Hero& hero);
-	
+	void addHeroToArch(const Hero& hero);
+
 	void printMarket() const;
 
 };

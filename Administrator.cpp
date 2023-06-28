0db822352188661ae67790275ec8ad91ba586bb3
Veliko Fotev <77541335+velik0o0@users.noreply.github.com>
2023-06-28 14:04:38 +0200

    Update Administrator.cpp

--- Administrator.cpp
+++ Administrator.cpp
@@ -1,36 +1,31 @@
-#include "Administrator.h"
-#include "Player.h"
-#include "Market.h"
-#include <iostream>
+#pragma once
+#include<cstring>
+#include<string>
 using namespace std;
+#pragma once
+#include "Player.h"
+#pragma warning (disable:4996)
+#include "User.h"
+#include "Market.h"
+#include <vector>
 
 
-Administrator::Administrator(const char* name, const char* email, const char* username, const char* password)
-    : User(name, email, username, password)
+class Administrator : public User
 {
-}
+public:
+	Administrator() = default;
+	Administrator(const char* name, const char* email, const char* username, const char* password);
 
+	/*void addAdmin(const Administrator& newAdministrator, std::vector<Administrator>& administrators);
+	void addPlayer(const Player& newPlayer, std::vector<Player>& players);
+	void addHeroToMarket(Market& market, const Hero& newHero);
+	void deletePlayer(Player& player);*/
+	
 
-bool Administrator::signIn(const char* inputUsername, const char* inputPassword) const
-{
-    std::string username(inputUsername);
-    std::string password(inputPassword);
+	bool signIn(const char* inputUsername, const char* inputPassword) const;
 
-    if (getUsername() == username && getPassword() == password) {
-        std::cout << getName() << ", you have successfully logged in as administrator!" << std::endl;
-        std::cin.ignore();//!!!
-        return true;
-    }
-    return false;
-}
-
-
-const char* Administrator::getAdministratorName() const
-{
-    return getName();
-}
-
-const char* Administrator::getAdministratorEmail() const
-{
-    return getEmail();
-}
+	//using User::name;   
+	//using User::email;   
+	const char* getAdministratorName() const;
+	const char* getAdministratorEmail() const;
+};
